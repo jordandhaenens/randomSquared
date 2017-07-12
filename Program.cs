5ebf3be5d87e1eb94bdc3bbecdd4e02f29eadc96
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-12 16:47:30 -0500

    changes before hitting the road

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace random
 {
@@ -6,7 +7,26 @@ namespace random
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = new List<int>();
+            
+            Random rando = new Random();
+            while (numbers.Count < 20)
+            {
+                numbers.Add(rando.Next(1, 50));
+            }
+
+            List<int> squared = new List<int>();
+            foreach (int num in numbers) {
+                squared.Add(num * num);
+            }
+
+            squared = squared.Where()
+            foreach (int num in squared) {
+                
+            }
+            foreach (int num in squared) {
+                Console.WriteLine(num);
+            }
         }
     }
 }
